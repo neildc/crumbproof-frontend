dc5166fb42b6c60a49e9cc0dafcd228154e209c9
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-10-09 17:20:21 +1100

    Activity Reducer for activity endpoints

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import RecipesReducer from "./reducer_recipes";
+import ActivitiesReducer from "./reducer_activities";
 import AuthReducer from "./reducer_auth";
 
 const rootReducer = combineReducers({
   recipes: RecipesReducer,
+  activities: ActivitiesReducer,
   auth: AuthReducer,
   form: formReducer
 });
