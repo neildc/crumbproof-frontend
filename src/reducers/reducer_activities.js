80c522cce13fa51ccb736f082ee7489289a065e4
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-10-10 12:30:38 +1100

    Reducers for activity endpoints

---
+++ src/reducers/reducer_activities.js
@@ -0,0 +1,15 @@
+import _ from "lodash";
+import { FETCH_ACTIVITIES, FETCH_ACTIVITY, DELETE_ACTIVITY } from "../actions";
+
+export default function(state = {}, action) {
+  switch (action.type) {
+  case DELETE_ACTIVITY:
+    return _.omit(state, action.payload);
+  case FETCH_ACTIVITY:
+    return { ...state, [action.payload.data.id]: action.payload.data };
+  case FETCH_ACTIVITIES:
+    return _.mapKeys(action.payload.data.results, "id");
+  default:
+    return state;
+  }
+}
