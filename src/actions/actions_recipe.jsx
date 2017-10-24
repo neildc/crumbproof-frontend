24a8201dc190344df027a4b16fc1854e87574fd3
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-10-24 12:44:58 +1100

    New reducer for fetching activities linked a certain recipe

--- src/actions/actions_recipe.jsx
+++ src/actions/actions_recipe.jsx
@@ -3,6 +3,7 @@ import { ROOT_URL } from "../constants/hosts";
 
 export const FETCH_RECIPES = "fetch_recipes";
 export const FETCH_RECIPE = "fetch_recipe";
+export const FETCH_RECIPE_ACTIVITIES = "fetch_recipe_activities";
 export const CREATE_RECIPE = "create_recipe";
 export const DELETE_RECIPE = "delete_recipe";
 
@@ -59,5 +60,14 @@ export function deleteRecipe(id, callback) {
   return {
     type: DELETE_RECIPE,
     payload: id
+  };
+}
+
+export function fetchRecipeActivities(id) {
+  const request = axios.get(`${ROOT_URL}/recipes/${id}/activities/`);
+
+  return {
+    type: FETCH_RECIPE_ACTIVITIES,
+    payload: request
   };
 }
