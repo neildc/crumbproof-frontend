f63e97f3afc9c65904f96059fa2f27f389ada1da
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-10-26 19:53:08 +1100

    New redux form component using MUI <AutoComplete>

---
+++ src/components/redux_form/auto_complete.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AutoComplete from 'material-ui/AutoComplete';
+
+export default function renderAutoComplete(field) {
+  const { meta: { touched, error } } = field;
+
+  return (
+    <div>
+      <AutoComplete
+        hintText=""
+        floatingLabelText={field.label}
+        errorText={touched && error}
+        {...field.input}
+        {...field.custom}
+        openOnFocus={true}
+        dataSource={field.suggestions}
+        filter={AutoComplete.fuzzyFilter}
+        maxSearchResults={3}
+    />
+    </div>
+  );
+}
