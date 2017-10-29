4de14705135d28625ce3b6ad1831a913fc4f0f04
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-10-29 11:08:34 +1100

    Insert keys to ingredients/instructions that were added to a recipe

--- src/util/diff.js
+++ src/util/diff.js
@@ -15,7 +15,6 @@ export function getModifications(type, orig, curr) {
   if (type === INSTRUCTIONS) { key = 'step_number';}
 
   let removed = _.differenceBy(orig, curr, key);
-  let added = _.filter(curr, i => !(key in i));
 
   let existedInOriginal = _.filter(curr, i => key in i);
 
@@ -24,5 +23,45 @@ export function getModifications(type, orig, curr) {
     return !equal(i, match);
   });
 
+  var added;
+  if (type === INGREDIENTS) {
+    added = _.filter(curr, i => !(key in i));
+
+    // Add negative ids from -1..-inf to any new ingredients
+    // guaranteed to not have any collisions with since existing
+    // ingredients will have a positive id
+    for (let i = 0; i < added.length; i++) {
+      added[i].id = (i+1)*(-1);
+    }
+  } else if (type === INSTRUCTIONS) {
+    let currWithStepNumbers = insertIntermediateStepNumbers(curr);
+    // All existing steps will have a integer for their step number
+    // New steps will have a decimal value for their step_number
+    added = _.filter(currWithStepNumbers, i => !_.isInteger(i.step_number));
+  }
+
   return {removed, added, modified};
+}
+
+function insertIntermediateStepNumbers(instructions) {
+
+  var ret = _.cloneDeep(instructions);
+
+  // Gives us 100 possible keys to use in between each step
+  // Should be plenty, unlikely that a user will add 100 steps
+  const STEP_INCREMENT = 0.01;
+
+  for (let i = 0; i < ret.length; i++) {
+
+    // No step number therefore it must be a modification
+    // to the original, since step numbers only get added to the
+    // instruction when a recipe is submitted
+    if (!ret[i].step_number) {
+
+      // The fractional increment gives us a way to distinguish it from
+      // original ret while retaining order
+      ret[i].step_number = ret[i-1].step_number + STEP_INCREMENT;
+    }
+  }
+  return ret;
 }
