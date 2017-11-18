59e2fc33c6652843cde2def8fd06ce905604ce2b
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-11-18 17:39:55 +1100

    Submit button can now be a FlatButton

--- src/components/submit_button.jsx
+++ src/components/submit_button.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import CircularProgress from 'material-ui/CircularProgress';
 
 export default class SubmitButton extends React.Component {
   render() {
     const {
-      submittingFlag, label, labelInProgress, ...rest
+      submittingFlag, label, labelInProgress, flat, ...rest
     } = this.props;
 
+    const ButtonComponent = flat ? FlatButton : RaisedButton;
+
     return (
-      <RaisedButton
+      <ButtonComponent
         {...rest}
         label={submittingFlag ? labelInProgress : label}
         labelPosition="before"
         disabled={submittingFlag}
         type="submit"
-        primary
+        primary={!flat}
         icon={submittingFlag &&
         <CircularProgress
           style={{ marginLeft: '20px' }}
@@ -33,4 +36,5 @@ SubmitButton.propTypes = {
   submittingFlag: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
   labelInProgress: PropTypes.string.isRequired,
+  flat: PropTypes.bool,
 };
