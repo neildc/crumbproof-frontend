fb2128e373a86d6597eb865a8f6dea8519e25363
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-11-21 18:57:19 +1100

    Snapshot tests for <Header/>

---
+++ src/components/header.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Header } from './header';
+
+it('renders without throwing', () => {
+  const component = shallow(
+    <Header />
+  );
+
+  expect(component).toMatchSnapshot();
+});
