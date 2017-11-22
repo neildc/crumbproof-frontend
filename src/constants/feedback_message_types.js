c85b3a3f50de28f138d2939c77a7394dee491c6f
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-11-22 16:25:32 +1100

    Pulled out the error message display from login/reigster to work globally

---
+++ src/constants/feedback_message_types.js
@@ -0,0 +1,3 @@
+export const ERROR_MSG = 'ERROR_MESSAGE';
+export const NORMAL_MSG = 'NORMAL_MESSAGE';
+export const NO_MSG = 'NO_MESSAGE';
--- src/reducers/reducer_auth.js
+++ src/reducers/reducer_auth.js
@@ -4,8 +4,9 @@ import {
   AUTH_LOGOUT,
   AUTH_REGISTER,
   AUTH_FORBIDDEN,
-  AUTH_CLEAR_ERROR,
 } from '../actions/actions_auth';
+
+import { CLEAR_FEEDBACK_MESSAGE } from '../actions/actions_feedback_messages';
 
 const DEFAULT_ERROR_MESSAGE = 'Please check your internet or try again later';
 
@@ -87,8 +88,12 @@ export default function (state = {}, action) {
     case AUTH_FORBIDDEN:
       return { error: action.payload };
 
-    case AUTH_CLEAR_ERROR:
-      return { ...state, error: null };
+    case CLEAR_FEEDBACK_MESSAGE:
+      if (action.payload === 'auth') {
+        return { ...state, error: null, message: null };
+      } else {
+        return state;
+      }
 
     case AUTH_CHECK_LOCAL_STORAGE:
       if (localStorage.getItem('token') !== null
