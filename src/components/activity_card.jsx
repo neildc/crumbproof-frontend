f220c92c1f086c1e370d06f5f30614aab8d513a7
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2018-03-11 15:33:45 +1100

    ActivityCard: Splitting up main render function

--- src/components/activity_card.jsx
+++ src/components/activity_card.jsx
@@ -32,6 +32,95 @@ export class ActivityCard extends Component {
     this.props.deleteActivity(this.props.activity.id, () => {});
   }
 
+  renderRecipeHeader(activity) {
+
+    const created = moment(activity.created).fromNow();
+
+    return(
+      <CardMedia
+        className="cardPhoto"
+        /*
+        *  Without this the overlay captures all click events
+        *  instead of the events being handled by the ImageZoom component
+        */
+        overlayContainerStyle={{ pointerEvents: 'none' }}
+        overlay={
+          <CardTitle
+            title={activity.name}
+                  subtitle={`${created} by ${activity.user}`}
+          />
+        }
+      >
+
+        <LazyLoad offset={200} once>
+          <ImageZoom
+            image={{
+              src: activity.crumb_shot,
+              alt: '',
+              className: 'img',
+              style: { maxWidth: '100%' },
+            }}
+          />
+        </LazyLoad>
+      </CardMedia>
+    )
+  }
+
+  renderMenu(deleteAction) {
+    return(
+      <IconMenu
+        iconButtonElement={<IconButton tooltip="Menu"><TripleDotIcon /></IconButton>}
+        anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+        targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+        className="menuButton"
+      >
+        <MenuItem
+          primaryText="Delete"
+          leftIcon={<DeleteIcon color="black" />}
+          onClick={deleteAction}
+        />
+      </IconMenu>
+    )
+  }
+
+  renderRecipeInfo(activity) {
+    const { recipe, notes, started, completed } = activity;
+
+    let duration = null;
+
+    if (started && completed) {
+      duration = moment(completed).diff(moment(started), 'hours');
+    }
+
+    return (
+      <div>
+        { recipe &&
+          <p>
+            <b>Recipe Used:</b> <Link to={`/recipes/${recipe.id}`}>{recipe.data.name}</Link>
+          </p>
+        }
+
+        { completed &&
+          <p>Completed: {completed}</p>
+        }
+
+        { duration &&
+          <p>Total duration: {duration} hours</p>
+        }
+
+        { notes &&
+          <p><b>Notes:</b> {notes}</p>
+        }
+
+        { recipe &&
+          <RecipeDiff
+            recipe={recipe}
+          />
+        }
+      </div>
+    )
+  }
+
   render() {
     const { activity } = this.props;
 
@@ -39,90 +128,18 @@ export class ActivityCard extends Component {
       return <ActivityCardPlaceholder />;
     }
 
-    let duration = null;
-
-    if (activity.completed && activity.started) {
-      duration = moment(activity.completed).diff(moment(activity.started), 'hours');
-    }
-
-    const created = moment(activity.created).fromNow();
-
 
     return (
       <Card style={this.props.style} >
 
-        <CardMedia
-          className="cardPhoto"
-          /*
-          *  Without this the overlay captures all click events
-          *  instead of the events being handled by the ImageZoom component
-          */
-          overlayContainerStyle={{ pointerEvents: 'none' }}
-          overlay={
-            <CardTitle
-              title={activity.name}
-              subtitle={`${created} by ${activity.user}`}
-            />
-          }
-        >
+        {this.renderRecipeHeader(activity)}
 
-          <LazyLoad offset={200} once>
-            <ImageZoom
-              image={{
-                src: activity.crumb_shot,
-                alt: '',
-                className: 'img',
-                style: { maxWidth: '100%' },
-              }}
-            />
-          </LazyLoad>
-        </CardMedia>
+        {(this.props.user === activity.user) &&
+          this.renderMenu(this.handleDeleteClick)
+        }
 
-        <div style={{ padding: '30px' }}>
-
-          {(this.props.user === activity.user) &&
-            <IconMenu
-              iconButtonElement={<IconButton tooltip="Menu"><TripleDotIcon /></IconButton>}
-              anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
-              targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-              className="menuButton"
-            >
-              <MenuItem
-                primaryText="Delete"
-                leftIcon={<DeleteIcon color="black" />}
-                onClick={this.handleDeleteClick}
-              />
-            </IconMenu>
-          }
-
-          { activity.recipe &&
-          <p>
-            <b>Recipe Used:</b> <Link to={`/recipes/${activity.recipe.id}`}>{activity.recipe.data.name}</Link>
-          </p>
-          }
-          {activity.oven_start && activity.oven_end &&
-            <p>In the oven: {moment(activity.oven_start).format('h:mm:ss a')} {' - '}
-              {moment(activity.oven_end).format('h:mm:ss a')}
-            </p>
-          }
-
-          { activity.completed &&
-            <p>Completed: {activity.completed}</p>
-          }
-
-          { duration &&
-            <p>Total duration: {duration} hours</p>
-          }
-
-          { activity.notes &&
-            <p><b>Notes:</b> {activity.notes}</p>
-          }
-
-          { activity.recipe &&
-            <RecipeDiff
-              recipe={this.props.activity.recipe}
-            />
-          }
+e       <div style={{ padding: '30px' }}>
+          {this.renderRecipeInfo(activity)}
         </div>
 
       </Card>
