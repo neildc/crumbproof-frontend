003b9d12ba45e77f91ca5810688b24184a8a7f93
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2018-03-11 18:56:28 +1100

    LiveActivityControl: Refactoring into a more declarative style

--- src/components/live_activity_control.jsx
+++ src/components/live_activity_control.jsx
@@ -21,6 +21,122 @@ import { FadeIn } from './animations/fade';
 
 import './live_activity_control.css';
 
+const isLastStep = (instructions, currStep) => {
+  const numSteps = instructions.length;
+  return (currStep + 1 >= numSteps);
+}
+
+const PageControls = ({currentStepInAnchor, currStep, movingToNextStep, instructions, recipe, nextClickHandler}) => (
+  <div className="pageControls">
+    <Tooltip
+      className="tooltip"
+      content="Feel free to scroll through the steps, only the next button actually changes your current step"
+    >
+      <FlatButton
+        label={window.innerWidth < 640 ?
+               `Step #${currStep + 1}` :
+               `Return to current step: ${currStep + 1}`}
+
+        labelStyle={{ fontSize: '22px', fontWeight: 400 }}
+        href={`#step${currStep + 1}`}
+        disabled={currentStepInAnchor === currStep + 1}
+        icon={<ReplayIcon />}
+      />
+    </Tooltip>
+
+    <SubmitButton
+      className="nextStepButton"
+      submittingFlag={movingToNextStep}
+      label={isLastStep(instructions, currStep) ? 'Submit Activity' : 'Next'}
+      labelInProgress={isLastStep(instructions, currStep) ? 'Submit Activity' : 'Saving...'}
+      labelStyle={{ verticalAlign: 'middle' }}
+      href={`#step${currStep + 2}`}
+      onClick={nextClickHandler}
+      fullWidth={window.innerWidth <= 640}
+    />
+  </div>
+)
+
+const Timer = ({ currStepInstruction, currStepStartTime, started, isSubmitting, startTimerHandler }) => (
+  <FadeIn>
+    <div className="timerContainer">
+      <Tooltip
+        className="tooltip"
+        content={[
+          'This timer is running remotely, you may close this page if you want.\n',
+          'The push notification for this timer may be early/late by at most 1 minute']}
+      >
+        {currStepStartTime ?
+          <CountdownText
+            instruction={currStepInstruction}
+            startTime={currStepStartTime}
+          />
+          :
+          <TimerStarter
+            instruction={currStepInstruction}
+            hasSubmitted={isSubmitting}
+            startTimerHandler={startTimerHandler}
+          />
+        }
+        <Divider className="timerDivider" />
+      </Tooltip>
+    </div>
+  </FadeIn>
+)
+
+
+const CountdownText = ({instruction, startTime}) => {
+  const renderer = ({
+    hours,
+    minutes,
+    seconds,
+    completed,
+  }) => {
+    if (completed) {
+      return 'Completed';
+    }
+
+    const h = zeroPad(hours);
+    const m = zeroPad(minutes);
+    const s = zeroPad(seconds);
+
+    return <span>{h}:{m}:{s}</span>;
+  };
+
+  return (
+    <div className="countdown">
+      <Countdown
+        date={Date.parse(startTime) + (instruction.time_gap_to_next * 60 * 1000)}
+        renderer={renderer}
+      />
+    </div>
+  )
+}
+
+
+const TimerStarter = ({instruction, hasSubmitted, startTimerHandler}) => {
+  const { hours, minutes } = minToHandM(instruction.time_gap_to_next);
+
+  const h = zeroPad(hours);
+  const m = zeroPad(minutes);
+
+  return (
+    <div>
+      <SubmitButton
+        fullWidth
+        flat
+        label="Start timer"
+        labelInProgress="Starting timer..."
+        submittingFlag={hasSubmitted}
+        onClick={startTimerHandler}
+      />
+      <div className="timer">
+        {`${h}:${m}:00`}
+      </div>
+    </div>
+  )
+}
+
 class LiveActivityControl extends Component {
   constructor() {
     super();
@@ -28,6 +144,8 @@ class LiveActivityControl extends Component {
       movingToNextStep: false,
       startingTimer: false,
     };
+    this.handleNextClick.bind(this);
+    this.handleStartTimerClick.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -48,13 +166,8 @@ class LiveActivityControl extends Component {
     }
   }
 
-  isLastStep(instructions) {
-    const numSteps = instructions.length;
-    return (this.props.liveActivity.current_step + 1 >= numSteps);
-  }
-
-  handleNextClick(recipe) {
-    if (this.isLastStep(recipe.data.instructions)) {
+  handleNextClick(recipe, currStep) {
+    if (isLastStep(recipe.data.instructions, currStep)) {
       this.props.history.push(`/activity/new/recipe/${recipe.id}`);
     } else {
       this.props.liveActivityNextStep();
@@ -67,115 +180,33 @@ class LiveActivityControl extends Component {
     this.setState({ startingTimer: true });
   }
 
-  renderTimerStart(instruction) {
-    const { hours, minutes } = minToHandM(instruction.time_gap_to_next);
-
-    const h = zeroPad(hours);
-    const m = zeroPad(minutes);
-
-    return (
-      <div>
-        <SubmitButton
-          fullWidth
-          flat
-          label="Start timer"
-          labelInProgress="Starting timer..."
-          submittingFlag={this.state.startingTimer}
-          onClick={() => this.handleStartTimerClick()}
-        />
-        <div className="timer">
-          {`${h}:${m}:00`}
-        </div>
-      </div>
-    );
-  }
-
-  renderTimer(instruction) {
-    const startTime = this.props.liveActivity.start_times[instruction.id];
-
-    if (!startTime) {
-      return this.renderTimerStart(instruction);
-    }
-
-    const renderer = ({
-      hours,
-      minutes,
-      seconds,
-      completed,
-    }) => {
-      if (completed) {
-        return 'Completed';
-      }
-
-      const h = zeroPad(hours);
-      const m = zeroPad(minutes);
-      const s = zeroPad(seconds);
-
-      return <span>{h}:{m}:{s}</span>;
-    };
-
-    return (
-      <div className="timer">
-        <Countdown
-          date={Date.parse(startTime) + (instruction.time_gap_to_next * 60 * 1000)}
-          renderer={renderer}
-        />
-      </div>
-    );
-  }
-
   render() {
     const { recipe } = this.props.liveActivity;
-    const currStep = this.props.liveActivity.current_step;
     const { instructions } = recipe.data;
+    const currStep = this.props.liveActivity.current_step;
+    const currStepInstruction = instructions[currStep];
+    const currStepStartTime = this.props.liveActivity.start_times[currStepInstruction.id];
 
     return (
       <Card className="liveController">
-        { instructions[currStep].time_gap_to_next &&
-          <FadeIn>
-            <div className="timerContainer">
-              <Tooltip
-                className="tooltip"
-                content={[
-                  'This timer is running remotely, you may close this page if you want.\n',
-                  'The push notification for this timer may be early/late by at most 1 minute']}
-              >
-                {this.renderTimer(instructions[currStep])}
-                <Divider className="timerDivider" />
-              </Tooltip>
-            </div>
+      { currStepInstruction.time_gap_to_next &&
+        <Timer
+          currStepInstruction={currStepInstruction}
+          currStepStartTime={currStepStartTime}
+          isSubmitting={this.state.startingTimer}
+          startTimerHandler={() => this.handleStartTimerClick()}
+        />
+      }
 
-          </FadeIn>
-        }
+      <PageControls
+        currentStepInAnchor={this.props.currentStepInAnchor}
+        currStep
+        movingToNextStep={this.state.movingToNextStep}
+        instructions
+        recipe
+        nextClickHandler={() => this.handleNextClick(recipe, currStep)}
+      />
 
-        <div className="pageControls">
-          <Tooltip
-            className="tooltip"
-            content="Feel free to scroll through the steps, only the next button actually changes your current step"
-          >
-            <FlatButton
-              label={window.innerWidth < 640 ?
-                     `Step #${currStep + 1}` :
-                     `Return to current step: ${currStep + 1}`}
-
-              labelStyle={{ fontSize: '22px', fontWeight: 400 }}
-              href={`#step${currStep + 1}`}
-              disabled={this.props.currentStepInAnchor === currStep + 1}
-              icon={<ReplayIcon />}
-            />
-          </Tooltip>
-
-          <SubmitButton
-            className="nextStepButton"
-            submittingFlag={this.state.movingToNextStep}
-            label={this.isLastStep(instructions) ? 'Submit Activity' : 'Next'}
-            labelInProgress={this.isLastStep(instructions) ? 'Submit Activity' : 'Saving...'}
-            labelStyle={{ verticalAlign: 'middle' }}
-            href={`#step${currStep + 2}`}
-            onClick={() => this.handleNextClick(recipe)}
-            fullWidth={window.innerWidth <= 640}
-          />
-        </div>
       </Card>
     );
   }
